1ceb5b06c6262b8745fb4050ab4d5dcee61534a6
Nick <zhangzyhack@126.com>
2014-08-19 18:13:27 +0800

    change to embedXcode

--- GPSdecoder/GPSdecoder.ino
+++ GPSdecoder/GPSdecoder.ino
@@ -38,7 +38,7 @@
 #endif
 
 // Include application, user and local libraries
-#include "LocalLibrary.h"
+#include "NEMAGPS.h"
 
 
 // Prototypes
@@ -54,24 +54,12 @@ uint8_t myLED;
 
 //
 // Brief	Setup
-// Details	Define the pin the LED is connected to
+
 //
 // Add setup code 
 void setup() {
-  // myLED pin number
-#if defined(ENERGIA) // All LaunchPads supported by Energia
-    myLED = RED_LED;
-#elif defined(DIGISPARK) // Digispark specific
-    myLED = 1; // assuming model A
-#elif defined(MAPLE_IDE) // Maple specific
-    myLED = BOARD_LED_PIN;
-#elif defined(WIRING) // Wiring specific
-    myLED = 15;
-#else // Arduino, chipKIT, Teensy specific
-    myLED = 13;
-#endif
-
-    pinMode(myLED, OUTPUT);     
+  
+    ;
 }
 
 //
@@ -80,6 +68,5 @@ void setup() {
 //
 // Add loop code 
 void loop() {
-    blink(myLED, 3, 333);
-    delay(1000);
+    ;
 }
